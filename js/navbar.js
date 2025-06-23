f7ee2092509251eda6dc84aadf6306d0b0662d0f
thiagovodmr <tvnicius782@gmail.com>
2025-06-22 21:42:19 -0300

    teste - componentização header html e js puro

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const btnMobile = document.querySelector('.btn-mobile');
-    const links = document.querySelector('.links');
+class NavComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <nav>
+        <div class="info">
+          <span class="name">Thiago Vinícius Oliveira de Matos Rodrigues</span>
+          <span class="role">Programador, Igarassu - PE</span>
+        </div>
+        <div class="container-mobile">
+          <button class="btn-mobile" aria-label="Abrir menu">
+            <img src="../img/bars-solid.svg" alt="Menu" />
+          </button>
+        </div>
+        <ul class="links">
+          <li><a href="../">Sobre mim</a></li>
+          <li><a href="../pages/formacao.html">Formação</a></li>
+          <li><a href="../pages/experiencia.html">Experiência</a></li>
+          <li><a href="../pages/portfolio.html">Portfólio</a></li>
+          <li><a href="../pages/contato.html">Contato</a></li>
+        </ul>
+      </nav>
+    `;
 
-    btnMobile.addEventListener('click', onClickButton);
+    const btnMobile = this.querySelector('.btn-mobile');
+    const links = this.querySelector('.links');
 
-    function onClickButton(){
-        links.classList.toggle('active');
-    }
-});
+    btnMobile.addEventListener('click', () => {
+      links.classList.toggle('active');
+    });
+  }
+}
+
+customElements.define('nav-component', NavComponent);
